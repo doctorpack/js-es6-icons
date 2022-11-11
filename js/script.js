90427d6a1be0ffbd238756c1d1732b970c27b7e6
doctorpack <enricopachera@gmail.com>
2022-11-11 16:46:32 +0100

    fix exercise with teacher help

--- js/script.js
+++ js/script.js
@@ -1,134 +1,181 @@
-let container =document.querySelector(".card")
 const arrIcons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'orange',
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
-		type: 'vegetable',
+		type: 'fruit',
 		family: 'fas',
-		color: 'green'
+		color: 'green',
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
-		type: 'vegetable',
+		type: 'fruit',
 		family: 'fas',
-		color: 'green'
+		color: 'green',
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: 'green',
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: 'green',
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'blue',
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'blue',
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
-		type: 'user',
+		type: 'cosa',
 		family: 'fas',
-		color: 'blue'
+		color: 'blue',
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
-		type: 'user',
+		type: 'alien',
 		family: 'fas',
-		color: 'blue'
-	}
+		color: 'blue',
+	},
 ];
-generatormembers();
-// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente le card 
 
-function generatormembers(){
-    // scorro con un ciclo for l'array di oggetti
-    for(let i = 0; i < arrIcons.length; i++){
-        // newCard contiene il codice HTML che andrò ad inserire nel file index.html
-        let newCard = 
-        `<div class="card">
-                    <h3>${arrIcons[i].name}</h3>
-                    <p>${arrIcons[i].prefix}</p>
-                    <p>${arrIcons[i].type}</p>
-                    <p>${arrIcons[i].family}</p>
-                    <p>${arrIcons[i].color}</p>
-        </div>`
-            // inserisco nel file HTML il contenuto di newcard
-        container.innerHTML += newCard;
-    }
+const eleIconsContainer = document.querySelector('#icons-container');
+const eleSelect = document.querySelector('#type-filter');
+
+populateSelect(arrIcons, eleSelect);
+randomizeColors(arrIcons);
+renderIcons(arrIcons, eleIconsContainer);
+eleSelect.addEventListener('change', filterIcons);
+
+
+
+
+function renderIcons(arrData, eleContainer) {
+	eleContainer.innerHTML = '';
+	arrData.forEach(objIcon => eleContainer.innerHTML += generateCard(objIcon));
+}
+
+function generateCard(objData) {
+	return `
+		<div class="box">
+			<i class="${objData.family} ${objData.prefix}${objData.name}" style="color:${objData.color}"></i>
+			<div class="title">${objData.name}</div>
+		</div>
+	`;
+}
+
+function populateSelect(arrData, eleSelect) {
+
+	const arrTypes = [];
+	arrData.forEach(objIcon => arrTypes.includes(objIcon.type) ? '' : arrTypes.push(objIcon.type));
+
+	console.log(arrTypes);
+
+	arrTypes.forEach(type => eleSelect.innerHTML += `<option value="${type}">${type}</option>`);
+}
+
+function filterIcons() {
+	const selectedType = this.value; 
+
+	// filtrare array
+	if (selectedType !== '') {
+		arrIconsFiltered = arrIcons.filter(objIcon => objIcon.type === selectedType)
+	} else {
+		arrIconsFiltered = arrIcons;
+	}
+	console.log(arrIconsFiltered);
+
+	
+	renderIcons(arrIconsFiltered, eleIconsContainer);
+}
+
+function randomizeColors(arrIcons) {
+	arrIcons.forEach(objIcon => objIcon.color = getRandomColor());
+}
+
+function getRandomColor() {
+	const acceptableChars = '0123456789ABCDEF';
+	let color = '#'
+	for (let i = 0; i < 6; i++) {
+		color += acceptableChars[getRandomInteger(0, 15)];
+	}
+	return color; 
+}
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
